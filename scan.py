6c3fb51a9cb7b1701bcde62ef4ad926e3a8ed401
Daniele Anselmo <daniele.anselmo91@gmail.com>
2023-03-27 14:54:47 +0200

    first

---
+++ scan.py
@@ -0,0 +1,26 @@
+# package imports
+from lxml import etree
+from subprocess import Popen
+
+# module imports
+import argparse
+import os
+
+
+# function definition
+def get_ip(machine_name):
+    pass
+
+
+# class definition
+class Scanner:
+    def __init__(self):
+        pass
+
+
+# main
+if __name__ == '__main__':
+    scan = Scanner()
+    print('hello')
+
+exit()
